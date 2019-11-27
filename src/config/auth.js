d4fc0c58208df725b134da7e6c33e9b5109edc74
mondroidz <monnippon@gmail.com>
2019-11-26 22:19:20 -0300

    OAuth

---
+++ src/config/auth.js
@@ -0,0 +1,5 @@
+module.exports = {
+    //
+    secret: " e89f6c0cf00681c05195623406cf4a41",
+    expiresIn: '7d',
+}
--- src/routes/professorasRoute.js
+++ src/routes/professorasRoute.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const router = express.Router()
 const controller = require("../controllers/professorasController")
+const authMiddleware = require('../middlewares/auth')
 
 router.get("/", controller.get)
+router.use(authMiddleware);
 router.get("/:id", controller.getById)
 router.post("/", controller.post)
 
